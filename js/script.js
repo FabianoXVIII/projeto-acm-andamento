c08e80172b2099459adb9a50430d09857642837f
FabianoXVIII <fabiano2244ilha@gmail.com>
2021-09-29 15:14:59 -0300

    second commit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+const menuItems = document.querySelectorAll('.menu a[href^="#"]')
+
+console.log(menuItems)
+
+menuItems.forEach(item => {
+    item.addEventListener('click', scrollToIdOnClick);
+})
+
+function scrollToIdOnClick(event) {
+    event.preventDefault(); // previne o padrão.
+    const element = event.target;
+    const id = element.getAttribute('href'); // pega o valor de href
+    const to = document.querySelector(id).offsetTop;  // pega a quantidade de px do topo até o id
+
+    window.scroll({
+        top: to - 80,
+        behavior: "smooth", // suave
+
+    })
+
+}
